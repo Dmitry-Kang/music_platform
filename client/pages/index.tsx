3148ead38c6a1731bac41af72c09bafa9805bb9f
Kangus <alexandrov.dp@mail.ru>
2023-07-29 23:57:19 +0600

    add navbar

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,13 +1,15 @@
 
 import { Button } from "@mui/material";
 import React from "react";
+import Navbar from "../components/Navbar";
 
 const Index = () => {
     return (
         <>
+            <Navbar/>
             <div className="center">
                 <h1>Главная страница</h1>
-                <h3>Здесь собраны лучшие треки!</h3>
+                <h3>Здесь собраны лучшие треки!</h3> 
             </div>
 
             <style jsx>
